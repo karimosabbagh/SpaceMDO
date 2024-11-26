ed8c5b97dd3daab448bae79e754268c461ba41aa
JyotitSachdeva <jyotit.sachdeva@mail.mcgill.ca>
2024-11-26 16:17:44 -0500

    updated constraints

--- Subsystems/S2_orbital_capture.m
+++ Subsystems/S2_orbital_capture.m
@@ -19,9 +19,9 @@ TOF_max = 500;              % days
 % r_p2                       % m
 % departure_date             % 'year-month-day'
 % arrival_date               % 'year-month-day'
+function [delta_v_capture, V_SC_arrival, S2_constraint] = orbital_capture_delta_v(e, m_SC, delta_m_d, r_p2, V_SC_departure, departure_date, arrival_date)
 
-
-function [delta_v_capture, V_SC_arrival] = orbital_capture_delta_v(e, m_SC,delta_m_d, r_p2, V_SC_departure, departure_date, arrival_date)
+ global R_earth G M_Earth M_Sun earth_orbital_data mars_orbital_data;
 
     % Extract Earth and Mars position and velocity for the exact departure and arrival dates
     departure_row = earth_result_table(earth_result_table.DepartureDate == departure_date, :);
@@ -40,31 +40,60 @@ function [delta_v_capture, V_SC_arrival] = orbital_capture_delta_v(e, m_SC,delta
     V_infinity_A = V_SC_arrival - V_Mars_arrival;
 
         % calculate eccentricity
-    e = ((2 * G * (M_mars + (m_SC - delta_m_d) / V_infinity_A^2) - r_p2) / ...
-        (2 * G * (M_mars + (m_SC - delta_m_d) / V_infinity_A^2) + r_p2);
+    e = ((2 * G * (M_mars + (m_SC - delta_m_d)) / V_infinity_A^2) - r_p2) / ...
+        ((2 * G * (M_mars + (m_SC - delta_m_d)) / V_infinity_A^2) + r_p2);
     
     % calculate delta v
     delta_v_capture = sqrt(V_infinity_A ^ 2 + 2 * G * (M_mars + (m_SC - delta_m_d)) / (r_p2)) + ... 
         sqrt(G * (M_mars + (m_SC - delta_m_d)) * (1 + e) / (r_p2));
 
+    % Evaluate constraints
+    S2_constraints = S2_evaluate_constraints(r_p2, e, V_SC_arrival, V_Mars_arrival, ...
+        departure_date, arrival_date);
+end 
 
-    % constraints
+function tof = determine_tof(departure_date, arrival_date)
+% Compute the Julian day number at 0 UT for departure and arrival dates, and the time of flight
+    
+    % Extract year, month, and day
+    departure_year = year(departure_date);
+    departure_month = month(departure_date);
+    departure_day = day(departure_dt);
 
-    % g1: Parking orbit constraint
-    g1_lower = r_p2 - r_p2_min;                       % r_p2 >= r_p2_min
-    g1_upper = r_p2_max - r_p2;                       % r_p2 <= r_p2_max
+    arrival_year = year(arrival_date);
+    arrival_month = month(arrival_date);
+    arrival_day = day(arrival_date);
+    
+    % Compute J0 values
+    jd1 = 367*departure_year - fix(7*(departure_year + fix((departure_month + 9)/12))/4) + ...
+        fix(275*departure_month/9) + departure_day + 1721013.5;
+    jd2 = 367*arrival_year - fix(7*(arrival_year + fix((arrival_month + 9)/12))/4) + ...
+        fix(275*arrival_month/9) + arrival_day + 1721013.5;
 
-    % g2: Hyperbolic excess velocity constraint
-    g2 = V_SC_arrival - V_Mars_arrival;                    % V_A^(v) - V_Mars_arrival > 0
+    % Determine TOF
+    tof = jd2 - jd1;
+end
 
-    % g3: Launch window constraint
-    % implicit in earth_result_table as the possible date ranges are already provided
+function S2_constraints = S2_evaluate_constraints(r_p2, V_SC_arrival, V_Mars_arrival, ...
+    departure_date, arrival_date)
 
-    % g4: Time of flight constraint
-    g4_tof_max = (t_A - t_D) - TOF_max;               % TOF_max >= t_A - t_D
-    g4_tof_min = TOF_min - (t_A - t_D);               % t_A - t_D >= TOF_min
+  % g1: Parking orbit constraint (r_p2_min <= r_p2 <= r_p2_max)
+    c1 = r_p2;                                                     
 
-    %g5: eccentricity constraint                      % 0 < e < 1
-    g5 
+    % g2: Hyperbolic excess velocity constraint (V_D^(v) - V_Earth > 0)
+    c2 = V_SC_capture - V_Mars_arrival;               
+
+    % g3: Launch window constraint (launch window start <= departure_date <= launch window end)
+    c3 = departure_date;
+    
+    % g4: Time of flight constraint; (tof_min <= t_A - t_D <= tof_max)
+    tof = determine_tof(departure_date, arrival_date);
+    c4 = tof;              
+
+    % g5: Eccentricity
+    c5 = e;
+
+    % Combine constraints
+    S1_constraints = [c1, c2, c3, c4, c5];
 
 end
